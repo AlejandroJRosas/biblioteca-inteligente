a4db74e2d3341e6be311e77ae7510c344233882a
Feredev <hector1567xd@gmail.com>
2023-03-22 22:07:36 -0400

    Creacion de tablas users & refactorizacion en responses

--- sql/database.sql
+++ sql/database.sql
@@ -1,1 +1,11 @@
 -- Aqui se pone el código SQL
+
+-- Creacion de tabla de usuarios
+CREATE TABLE `users`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `role` ENUM('guest', 'client', 'admin') NOT NULL,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
