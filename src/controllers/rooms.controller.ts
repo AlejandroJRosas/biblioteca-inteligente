be010d145440b52d11c9e47b86f28fbc1c865088
Alejandro <alejandrojrosas2001@gmail.com>
2023-03-25 12:28:35 -0400

    Refactorización de api

--- src/controllers/rooms.controller.ts
+++ src/controllers/rooms.controller.ts
@@ -2,37 +2,68 @@ import { Request, Response } from 'express'
 import pool from '../database'
 import {
   PaginateSettings,
-  errorResponse,
   paginatedItemsResponse,
   successItemsResponse,
   successResponse
 } from '../utils/responses'
 import { RoomsRequestBody } from '../types/rooms'
 import { parseName } from '../utils/parsers'
+import StatusError from '../types/status-error'
+import { handleControllerError } from '../utils/handleControllerError'
 
-const MARIADB_STATUSCODES = 1000
-const INTERNAL_SERVER_ERROR = 'Ha ocurrido un error interno del servidor.'
-const PER_PAGE = 10
+const STATUS_OK = 200
+const STATUS_CREATED = 201
+const STATUS_BAD_REQUEST = 400
+const STATUS_NOT_FOUND = 404
+
+const DEFAULT_PAGE = 0
+const DEFAULT_SIZE = 10
+
+const validatePageAndSize = (
+  page: any,
+  size: any
+): [number, number] | string => {
+  let pageAsNumber: number
+  let sizeAsNumber: number
+
+  if (!isNaN(Number(page)) && Number.isInteger(Number(page))) {
+    pageAsNumber = Number.parseInt(page)
+  } else {
+    return 'La página debe ser un número entero'
+  }
+
+  if (!isNaN(Number(size)) && Number.isInteger(Number(size))) {
+    sizeAsNumber = Number.parseInt(size)
+  } else {
+    return 'La tamaño debe ser un número entero'
+  }
+
+  return [pageAsNumber, sizeAsNumber]
+}
 
 export const getRooms = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const { page = 0, size = PER_PAGE } = req.query
-  let [pageAsNumber, sizeAsNumber] = [
-    Number.parseInt(page as string),
-    Number.parseInt(size as string)
-  ]
-  if (pageAsNumber < 1) {
-    pageAsNumber = 1
-  }
-  const offset = (pageAsNumber - 1) * sizeAsNumber
+  const { page = DEFAULT_PAGE, size = DEFAULT_SIZE } = req.query
+  const validatedParams = validatePageAndSize(page, size)
+
   try {
+    if (typeof validatedParams === 'string') {
+      throw new StatusError(validatedParams, STATUS_BAD_REQUEST)
+    }
+
+    const [pageAsNumber, sizeAsNumber] = validatedParams
+
+    let offset = (pageAsNumber - 1) * sizeAsNumber
+
+    if (pageAsNumber < 1) {
+      offset = 0
+    }
+
     const [rows] = await pool.query('SELECT * FROM rooms')
     if (rows.length === 0) {
-      throw Object.assign(new Error('La tabla está vacía'), {
-        status: 404
-      })
+      throw new StatusError('La tabla está vacía', STATUS_NOT_FOUND)
     }
     const [result] = await pool.query('SELECT * FROM rooms LIMIT ?, ?', [
       offset,
@@ -43,12 +74,9 @@ export const getRooms = async (
       currentPage: pageAsNumber,
       perPage: sizeAsNumber
     }
-    return paginatedItemsResponse(res, 200, result, pagination)
-  } catch (error: any) {
-    if (error.status >= MARIADB_STATUSCODES) {
-      return errorResponse(res, 500, INTERNAL_SERVER_ERROR)
-    }
-    return errorResponse(res, error.status, error.message)
+    return paginatedItemsResponse(res, STATUS_OK, result, pagination)
+  } catch (error: unknown) {
+    return handleControllerError(error, res)
   }
 }
 
@@ -62,27 +90,20 @@ export const getRoomById = async (
       req.params.roomId
     )
     if (rows.length === 0) {
-      throw Object.assign(
-        new Error(
-          'No se pudo encontrar el registro de id:' + req.params.roomId
-        ),
-        {
-          status: 404
-        }
+      throw new StatusError(
+        `No se pudo encontrar el registro de id: ${req.params.roomId}`,
+        STATUS_NOT_FOUND
       )
     }
-    return successResponse(res, 200, rows[0])
-  } catch (error: any) {
-    if (error.status >= MARIADB_STATUSCODES) {
-      return errorResponse(res, 500, 'Internal Server Error')
-    }
-    return errorResponse(res, error.status, error.message)
+    return successResponse(res, STATUS_OK, rows[0])
+  } catch (error: unknown) {
+    return handleControllerError(error, res)
   }
 }
 
-const getRoomDataFromRequestBody = (object: any): RoomsRequestBody => {
+const getRoomDataFromRequestBody = (requestBody: any): RoomsRequestBody => {
   const newRoom: RoomsRequestBody = {
-    name: parseName(object.name)
+    name: parseName(requestBody.name)
   }
   return newRoom
 }
@@ -99,12 +120,9 @@ export const addRoom = async (
     const [roomItem] = await pool.query(
       `SELECT * FROM rooms WHERE id = ${insertedId}`
     )
-    return successItemsResponse(res, 201, roomItem[0])
-  } catch (error: any) {
-    if (error.status >= MARIADB_STATUSCODES) {
-      return errorResponse(res, 500, INTERNAL_SERVER_ERROR)
-    }
-    return errorResponse(res, error.status, error.message)
+    return successItemsResponse(res, STATUS_CREATED, roomItem[0])
+  } catch (error: unknown) {
+    return handleControllerError(error, res)
   }
 }
 
@@ -119,28 +137,21 @@ export const updateRoom = async (
       req.params.roomId
     ])
     if (rows.affectedRows === 0) {
-      throw Object.assign(
-        new Error(
-          'No se pudo encontrar el registro de id:' + req.params.roomId
-        ),
-        {
-          status: 404
-        }
+      throw new StatusError(
+        `No se pudo encontrar el registro de id: ${req.params.roomId}`,
+        STATUS_NOT_FOUND
       )
     }
     if (rows.changedRows === 0) {
       return successResponse(
         res,
-        200,
+        STATUS_OK,
         'Operación PUT exitosa pero el contenido del registro no cambió'
       )
     }
-    return successResponse(res, 200, 'Room modified')
-  } catch (error: any) {
-    if (error.status >= MARIADB_STATUSCODES) {
-      return errorResponse(res, 500, INTERNAL_SERVER_ERROR)
-    }
-    return errorResponse(res, error.status, error.message)
+    return successResponse(res, STATUS_OK, 'Sala modificada exitosamente')
+  } catch (error: unknown) {
+    return handleControllerError(error, res)
   }
 }
 
@@ -154,20 +165,13 @@ export const deleteRoom = async (
       req.params.roomId
     )
     if (rows.affectedRows === 0) {
-      throw Object.assign(
-        new Error(
-          'No se pudo encontrar el registro de id:' + req.params.roomId
-        ),
-        {
-          status: 404
-        }
+      throw new StatusError(
+        `No se pudo encontrar el registro de id: ${req.params.roomId}`,
+        STATUS_NOT_FOUND
       )
     }
-    return successResponse(res, 200, 'Sala eliminada')
-  } catch (error: any) {
-    if (error.status >= MARIADB_STATUSCODES) {
-      return errorResponse(res, 500, INTERNAL_SERVER_ERROR)
-    }
-    return errorResponse(res, error.status, error.message)
+    return successResponse(res, STATUS_OK, 'Sala eliminada')
+  } catch (error: unknown) {
+    return handleControllerError(error, res)
   }
 }
--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,10 +1,15 @@
+import StatusError from '../types/status-error'
+
 export const isString = (string: string): boolean => {
   return typeof string === 'string'
 }
 
 export const parseName = (nameFromRequest: any): string => {
   if (!isString(nameFromRequest)) {
-    throw Object.assign(new Error('Incorrect or missing name'), { status: 400 })
+    throw new StatusError(
+      'El campo [name] es requerido y debe ser una cadena no vacía',
+      400
+    )
   }
 
   return nameFromRequest
@@ -16,7 +21,10 @@ export const isDate = (date: string): boolean => {
 
 export const parseDate = (dateFromRequest: any): string => {
   if (!isString(dateFromRequest) || !isDate(dateFromRequest)) {
-    throw Object.assign(new Error('Incorrect or missing date'), { status: 400 })
+    throw new StatusError(
+      'El campo [date] es requerido y debe ser un valor de tipo date no vacío',
+      400
+    )
   }
 
   return dateFromRequest
